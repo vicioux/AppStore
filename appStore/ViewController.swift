b753e5cff9a8ad57598f631e8221d7bb57dbfefb
Sergio A. Orozco <Kashell@MacBook-Pro-de-Alejandro.local>
2016-04-10 08:39:24 -0500

    once upon a time

---
+++ appStore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appStore
+//
+//  Created by Alejandro Orozco Builes on 10/04/16.
+//  Copyright © 2016 vicioux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
